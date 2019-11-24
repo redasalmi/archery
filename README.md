98bb8cb2eabfe0bf354c9b3826b97bed732f6d3d
redasalmi <reda.salmi.elt@gmail.com>
2019-11-24 13:49:51 +0100

    readme and redirects

---
+++ README.md
@@ -0,0 +1,25 @@
+# ARCHERY
+
+A website About Archery with definitions, articles and videos to know more about Archery. found at: [https://archerie.netlify.com/](https://archerie.netlify.com/)
+
+## Built With
+
+- React
+- react-router-dom
+- react-transition-group
+- Bootstrap
+
+## Cloning & Installation
+
+To clone this project and start it, open a terminal and follow this steps:
+
+```
+	git clone https://github.com/redasalmi/archery.git
+	cd archery
+	npm install
+	npm start
+```
+
+**Note:**
+* `npm install` will install all the dependencies needed to run the project on your computer.
+* `npm start` will open your default browser and start the website on your localhost.
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -5,7 +5,7 @@ import ARCHERY from "../data/archery";
 const VideoFrame = ({ source }) => {
   return ARCHERY.videos[source].map(video => {
     let videoUrl = `https://www.youtube.com/embed/${video.id}`;
-    let imgUrl = `http://i3.ytimg.com/vi/${video.id}/hqdefault.jpg`;
+    let imgUrl = `https://i3.ytimg.com/vi/${video.id}/hqdefault.jpg`;
     let srcDoc = `<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
     span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
     </style><a href=${videoUrl}?autoplay=1><img src=${imgUrl} alt=${video.title}><span>▶</span></a>`;
