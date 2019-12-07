348384dae7cd2a8bc5ed17f75a1124c6a65334ac
redasalmi <reda.salmi.elt@gmail.com>
2019-12-07 09:39:03 +0100

    fetching from archery api

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row } from "reactstrap";
-import ARCHERY from "../data/archery";
+import axios from "axios";
 
-const ArticleLink = ({ source }) => {
-  return ARCHERY.articles[source].map(article => (
-    <div
-      className="col-12 col-sm-6 col-lg-4"
-      key={article.title}
-      style={{ minHeight: "250px" }}
-    >
-      <a href={article.link} rel="noopener noreferrer" target="_blank">
-        <img
-          src={article.img}
-          alt={article.title}
-          className="border border-dark rounded article-img"
-        />
-        <p className="text-dark font-weight-bold article-title">
-          {article.title}
-        </p>
-      </a>
-    </div>
-  ));
+const ArticleLink = ({ articles }) => {
+  if (articles === "error") {
+    return (
+      <div>
+        <h4 className="text-primary">
+          Sorry, Somethin is wrong with the API, try to refresh the page later.
+        </h4>
+      </div>
+    );
+  } else {
+    return articles.map(article => (
+      <div
+        className="col-12 col-sm-6 col-lg-4"
+        key={article.title}
+        style={{ minHeight: "250px" }}
+      >
+        <a href={article.link} rel="noopener noreferrer" target="_blank">
+          <img
+            src={article.img}
+            alt={article.title}
+            className="border border-dark rounded article-img"
+          />
+          <p className="text-dark font-weight-bold article-title">
+            {article.title}
+          </p>
+        </a>
+      </div>
+    ));
+  }
 };
 
 const Articles = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        const result = await axios(
+          "https://archery-api.now.sh/api/archery/articles"
+        );
+        setArticles(result.data);
+      } catch (error) {
+        setArticles("error");
+      }
+    }
+    fetchArticles();
+  }, []);
+
   return (
     <div className="m-4">
       <Row>
@@ -36,8 +61,7 @@ const Articles = () => {
         </div>
       </Row>
       <Row className="text-center mt-4">
-        <ArticleLink source="World Archery" />
-        <ArticleLink source="Archery 360" />
+        <ArticleLink articles={articles} />
       </Row>
     </div>
   );
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row } from "reactstrap";
-import ARCHERY from "../data/archery";
+import axios from "axios";
 
-const VideoFrame = ({ source }) => {
-  return ARCHERY.videos[source].map(video => {
-    let videoUrl = `https://www.youtube.com/embed/${video.id}`;
-    let imgUrl = `https://i3.ytimg.com/vi/${video.id}/hqdefault.jpg`;
-    let srcDoc = `<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
-    span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
-    </style><a href=${videoUrl}?autoplay=1><img src=${imgUrl} alt=${video.title}><span>▶</span></a>`;
+const VideoFrame = ({ videos }) => {
+  if (videos === "error") {
     return (
-      <div className="col-12 col-md-6" key={video.title}>
-        <div className="embed-responsive embed-responsive-16by9">
-          <iframe
-            title={video.title}
-            src={videoUrl}
-            srcDoc={srcDoc}
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen="allowFullScreen"
-            className="border border-dark embed-responsive-item"
-          ></iframe>
-        </div>
-        <p className="font-weight-bold">{video.title}</p>
+      <div>
+        <h4 className="text-primary">
+          Sorry, Somethin is wrong with the API, try to refresh the page later.
+        </h4>
       </div>
     );
-  });
+  } else {
+    return videos.map(video => {
+      let videoUrl = `https://www.youtube.com/embed/${video.id}`;
+      let srcDoc = `<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
+      span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
+      </style><a href=${videoUrl}?autoplay=1><img src=${video.thumbnail} alt=${video.title}><span>▶</span></a>`;
+      return (
+        <div className="col-12 col-md-6" key={video.title}>
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe
+              title={video.title}
+              src={videoUrl}
+              srcDoc={srcDoc}
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen="allowFullScreen"
+              className="border border-dark embed-responsive-item"
+            ></iframe>
+          </div>
+          <p className="font-weight-bold">{video.title}</p>
+        </div>
+      );
+    });
+  }
 };
 
 const Videos = () => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchVideos() {
+      try {
+        const result = await axios(
+          "https://archery-api.now.sh/api/archery/videos"
+        );
+        setVideos(result.data);
+      } catch (error) {
+        setVideos("error");
+      }
+    }
+    fetchVideos();
+  }, []);
+
   return (
     <div className="m-4">
       <Row>
@@ -40,8 +64,7 @@ const Videos = () => {
         </div>
       </Row>
       <Row className="text-center mt-4">
-        <VideoFrame source="World Archery" />
-        <VideoFrame source="Archery 360" />
+        <VideoFrame videos={videos} />
       </Row>
     </div>
   );
